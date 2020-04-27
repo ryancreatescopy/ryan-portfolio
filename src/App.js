7985191ac66ba5fb7fbe712356ba35f9df4abc96
Ryan Cordell <ryan.cordell@consensys.net>
2020-04-27 19:38:56 +0100

    made heading pt smaller on mobile

--- src/App.js
+++ src/App.js
@@ -51,9 +51,9 @@ const App = () => {
           <Container>
           <Box>
               <Box pt={5}>
-                  <Image mt={[6, 5]} src="/portfolio-header-image.jpg" />
+                  <Image mt={5} src="/portfolio-header-image.jpg" />
                 <Flex justifyContent="center">
-                  <Heading color="#1A2233" as={"h1"} fontSize={7} pt={5} pb={3}>
+                  <Heading color="#1A2233" as={"h1"} fontSize={7} pt={[4, 5]} pb={3}>
                     Hi I’m Ryan, content designer and doer
                   </Heading>
                 </Flex>
